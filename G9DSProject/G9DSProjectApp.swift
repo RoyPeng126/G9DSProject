3447d2e581dd1eb22c60dfbb4fe40a158c933665
RoyPeng126 <147261778+RoyPeng126@users.noreply.github.com>
2024-12-22 13:58:50 +0800

    IOSTest1

--- G9DSProject/G9DSProjectApp.swift
+++ G9DSProject/G9DSProjectApp.swift
@@ -1,17 +1,18 @@
-//
-//  G9DSProjectApp.swift
-//  G9DSProject
-//
-//  Created by 彭宇承 on 2024/12/22.
-//
-
 import SwiftUI
 
 @main
 struct G9DSProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SearchViewControllerWrapper()
         }
     }
+}
+
+struct SearchViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> SearchViewController {
+        return SearchViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: SearchViewController, context: Context) {}
 }
